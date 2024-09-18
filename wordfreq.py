71f84c6873a8e0d38294acfd9ece2ebba3eb92ba
Sixten Paulsson <sixtenpaulsson@gmail.com>
2024-09-18 14:20:40 +0200

    more progress on tokenize

--- wordfreq.py
+++ wordfreq.py
@@ -1,3 +1,13 @@
+#Funktion för att tokenizea 
 def tokenize(sentences=[]):
-    for x in sentences:
-        print(x)
+    term_list = []
+    #Checkar ifall 
+    for sentence in sentences:
+        #Går igenom varenda mening
+        words=sentence.split()
+        print(sentence)
+        for word in words:
+            #print(word)
+            term_list.append(word)
+    print(term_list)
+    return term_list
