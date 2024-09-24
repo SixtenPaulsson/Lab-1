340ceac36bbea3eedec5c2e0e11dc27691fe1227
Sixten Paulsson <sixtenpaulsson@gmail.com>
2024-09-24 17:48:36 +0200

    tokenize now working

--- wordfreq.py
+++ wordfreq.py
@@ -8,16 +8,13 @@ def tokenize(lines=[]):
         while start < len(line):
             #print(line[start])
             if(line[start].isalpha() or line[start].isdigit()):
-                print(line[start])
+                #print(line[start])
                 end = start
-                while end < len(line) and line[end].isalpha()==line[start].isalpha():
+                while end < len(line) and line[end].isalpha()==line[start].isalpha() and line[end].isdigit()==line[start].isdigit():
                     end+=1
-                print(line[start:end])
-                
-                word_list.append(line[start:end])
-                start=end
-
-
+                #print(line[start:end])
+                word_list.append(line[start:end].lower())
+                start=end-1
             elif(not line[start].isspace()):
                 word_list.append(line[start])
             start = start+1
@@ -38,16 +35,20 @@ def countWords(word_list=[],stop_words=[]):
         word_list=remove_items(word_list,stop_word)
     counted_words={}
     for word in word_list:
-        if not word in counted_words:
+        if counted_words.get(word) == None:
             counted_words[word]=0
         counted_words[word] +=1
     return counted_words
 
 
 
-def remove_items(test_list, item): 
-    res = [i for i in test_list if i != item] 
+def remove_items(test_list, stopword): 
+    res = [i for i in test_list if i != stopword] 
+    #if(test_list.conti)
     return res 
+
+
+
 
 def printTopMost(counted_words={},printed_amount=0):
     sorted_list = sorted(counted_words.items(), key=lambda x: -x[1])
@@ -55,8 +56,4 @@ def printTopMost(counted_words={},printed_amount=0):
         if(i==len(sorted_list)):
            break
         print(sorted_list[i][0].ljust(20)+str(sorted_list[i][1]).rjust(5))
-
-
-
-    
-    
+#print(tokenize(["the 10 little chicks"]))
