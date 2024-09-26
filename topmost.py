a6f2bc3f493555d8386c2ce501778db632605b6d
Sixten Paulsson <Sixten Paulsson>
2024-09-26 12:59:19 +0200

    topmost progress

--- topmost.py
+++ topmost.py
@@ -1,5 +1,11 @@
 import wordfreq
+import sys
+with open(sys.argv[1], encoding="utf-8") as inp_file:
+    for line in inp_file:
+        # do something with the current line
+        print(wordfreq.tokenize(line))
 
 
-asd = open("eng_stopwords.txt",encoding="utf-8")
-stopwords=asd.read().split("\n")
+
+#asd = open("eng_stopwords.txt",encoding="utf-8")
+#stopwords=asd.read().split("\n")
