bd26f9f0ad32f184e26807c1eba821000bee4946
Sixten Paulsson <Sixten Paulsson>
2024-09-26 14:31:44 +0200

    mer kommentarer

--- topmost.py
+++ topmost.py
@@ -2,17 +2,18 @@ import wordfreq
 import sys
 import urllib.request
 
-
-#python3 topmost.py eng_stopwords.txt examples/article1.txt 20
-#python3 topmost.py eng_stopwords.txt https://www.gutenberg.org/files/15/15-0.txt 20
 def main():
-    if(sys.argv[2].startswith("http") or sys.argv[2].startswith("https")):
+    #Ifall det är en hemsida som ska checkas
+    if(sys.argv[2].startswith("http")):
         response = urllib.request.urlopen(sys.argv[2])
         words = wordfreq.tokenize(response.read().decode("utf8").splitlines())
+    #Ifall det är en text som ska checkas
     else:
         with open(sys.argv[2], encoding="utf-8") as inp_file:
             words = wordfreq.tokenize(inp_file)
+    #Hämtar ut alla stopwords
     stopwords=open(sys.argv[1],encoding="utf-8").read().split("\n")
+    #Printar de ord som är med mest i texten
     wordfreq.printTopMost(wordfreq.countWords(words,stopwords),int(sys.argv[3]))
 
 if __name__ == "__main__":
--- wordfreq.py
+++ wordfreq.py
@@ -1,14 +1,19 @@
 def tokenize(lines=[]):
-    #Word list är listan på orden
+    #Lista på alla ord
     word_list = []
     for line in lines:
         start = 0
         while start < len(line):
+
             #Kollar ifall bokstaven är bokstav eller nummer
             if(line[start].isalnum()):
-                end = start
+
+                end = start + 1
+                #Gör så att end blir indexet på när ordet slutar
+                #Efteråt kommer line[start:end] bli ett ord
                 while end < len(line) and line[end].isalpha()==line[start].isalpha() and line[end].isdigit()==line[start].isdigit():
                     end+=1
+                
                 #Lägger till ordet i listan
                 word_list.append(line[start:end].lower())
                 start=end-1
